36e15e548f09d0ed3a61e8a06798f59df61c9c39
mahmudh <mahmudhssn246@gmail.com>
2016-02-28 20:03:53 +0000

    setting routes in express

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
 var express = require('express');
 var app = express();
 
-port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
+
 
 app.use(express.static('public'));
 /*app.use(express.static('src/views'));*/
 app.set('views', './src/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
+var books = [
+    {
+        title: 'War and Peace',
+        genre: 'Historical Fiction',
+        author: 'Lev Nikolayevich',
+        read: false
+    },
+    {
+        title: 'Lost Ark',
+        genre: 'Fantasy',
+        author: 'Nemo',
+        read: false
+    }
+];
+
+bookRouter.route('/').get(function(req, res) {
+    res.render("books", { title: 'Books', 
+    nav: [
+        {Link: '/Books', Text: 'Books'}, 
+        {Link: '/Authors', Text: 'Authors'}],
+    books: books
+      });
+});
+
+bookRouter.route('/single').get(function(req, res) {
+    res.send("Hello Single Book");
+}); 
+
+app.use('/Books', bookRouter);
 
 app.get('/', function(req, res){
-  res.render('index');
+    res.render('index', { nav: [
+        {Link: '/Books', Text: 'Books'}, 
+        {Link: '/Authors', Text: 'Authors'}] 
+      });
 });
 
 app.get('/books', function(req, res){
