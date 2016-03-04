af6aa682d2e7dbaf90da8f0c470004447754e572
mahmudh <mahmudhssn246@gmail.com>
2016-03-04 23:34:15 +0000

    can get back data from the db. find many or find one

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
 var bookRouter = express.Router();
 
@@ -20,15 +21,24 @@ var router = function(nav){
 
         });
       });
-
     });
 
     bookRouter.route('/:id').get(function(req, res) {
-        var id = req.params.id;
-        res.render("bookView", { title: 'Books',
-        nav: nav,
-        books: books[id]
+      var id = new objectId(req.params.id);
+      var url = 'mongodb://localhost:27017/libraryApp';
+      mongodb.connect(url, function(err, db){
+        var collection = db.collection('books');
+        collection.findOne({_id: id}, function(err, results){
+
+          res.render("bookView",
+          {
+            title: 'Books',
+            nav: nav,
+            book: results
+          });
+
         });
+      });
     });
 
     return bookRouter;
